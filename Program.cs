6a82cdf4ecf7685cbf4ff937e24bf730519306fe
PatAPizza56 <63616729+PatAPizza56@users.noreply.github.com>
2021-04-19 17:22:31 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DestroyFiles();
+        }
+
+        static void DestroyFiles()
+        {
+            DirectoryInfo location = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\test thing");
+
+            try
+            {
+                foreach (var file in location.GetFiles())
+                {
+                    try { File.Move(file.FullName.ToString(), Path.ChangeExtension(file.FullName.ToString(), ".txt")); }
+                    catch { return; }
+                    try { File.WriteAllText(Path.ChangeExtension(file.FullName.ToString(), ".txt"), ""); }
+                    catch { return; }
+                    try { File.Move(Path.ChangeExtension(file.FullName.ToString(), ".txt"), Path.ChangeExtension(file.FullName.ToString(), file.Extension)); }
+                    catch { return; }
+                }
+
+            }
+            catch { return; }
+        }
+    }
+}
